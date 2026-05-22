04f937765a77274f1b8a758a5bc4bba66b7a82dc
merlin32 <state.tudor2005@gmail.com>
2026-05-23 00:28:14 +0300

    Etapa 6 completed

--- resurse/js/schimba_tema.js
+++ resurse/js/schimba_tema.js
@@ -1,12 +1,20 @@
 window.addEventListener("DOMContentLoaded", function(){
-document.getElementById("theme-button").onclick= function(){
-    if(document.body.classList.contains("light")){
-        document.body.classList.remove("light")
-        localStorage.removeItem("tema");
+    let switchTema = document.getElementById("flexSwitchCheckDefault");
+
+    if (switchTema) {
+        if (localStorage.getItem("tema") === "light") {
+            switchTema.checked = true;
+            document.body.classList.add("light");
+        }
+
+        switchTema.onchange = function() {
+            if (this.checked) {
+                document.body.classList.add("light");
+                localStorage.setItem("tema", "light");
+            } else {
+                document.body.classList.remove("light");
+                localStorage.removeItem("tema");
+            }
+        }
     }
-    else{
-        document.body.classList.add("light")
-        localStorage.setItem("tema","light");
-    }
-}
 });
